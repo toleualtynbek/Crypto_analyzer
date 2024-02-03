f6cb94c757efb7ee05cedf528fbb64779355fc29
toleu.altinbek <toleu.altinbek@gmail.com>
2024-02-03 15:42:31 +0600

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import Service.CryptoService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        CryptoService cryptoService = new CryptoService();
+        Scanner scanner = new Scanner(System.in);
+        int value = 1;
+
+        switch (value)
+        {
+            case 1:
+            {
+                System.out.println("Введите имя файла, который нужно зашифровать");
+                String path_1 = scanner.nextLine();
+
+                System.out.println("Введите имя файла куда сложить результат");
+                String path_2 = scanner.nextLine();
+
+                System.out.println("Введите ключь");
+                int key = scanner.nextInt();
+
+                cryptoService.crypt(path_1,path_2,key);
+            }
+        }
     }
 }
---
+++ src/Service/CryptoService.java
@@ -0,0 +1,9 @@
+package Service;
+
+public class CryptoService
+{
+    public void crypt(String path1, String path2, int key)
+    {
+
+    }
+}
